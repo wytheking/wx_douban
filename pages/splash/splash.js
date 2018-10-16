d5302e8e6a970562db27eb718aa6ca79a2bb0d06
wythe <wythe523@163.com>
2018-10-16 17:18:48 +0800

    新增启动页

--- pages/splash/splash.js
+++ pages/splash/splash.js
@@ -1,66 +1,60 @@
-// pages/splash/splash.js
-Page({
+// 获取全局应用程序实例对象
+const app = getApp()
 
+// 创建页面实例对象
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-  
+    movies: [],
+    loading: true
+  },
+
+  getCache() {
+    return new Promise(resolve => {
+      app.wechat.getStorage('last_splash_data')
+        .then(res => {
+          const { movies, expires } = res.data
+          // 有缓存，判断是否过期
+          if (movies && expires > Date.now()) {
+            return resolve(res.data)
+          }
+          // 已经过期
+          console.log('uncached')
+          return resolve(null)
+        })
+        .catch(e => resolve(null))
+    })
+  },
+
+  handleStart() {
+    // TODO: 访问历史的问题
+    wx.switchTab({
+      url: '../board/board'
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
-  },
+  onLoad() {
+    this.getCache()
+      .then(cache => {
+        if (cache) {
+          return this.setData({ movies: cache.movies, loading: false })
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+        app.douban.find('coming_soon', 1, 3)
+          .then(d => {
+            this.setData({ movies: d.subjects, loading: false })
+            return app.wechat.setStorage('last_splash_data', {
+              movies: d.subjects,
+              expires: Date.now() + 1 * 24 * 60 * 60 * 1000
+            })
+          })
+          .then(() => console.log('storage last splash data'))
+      })
   }
+  
 })
